8d456f6d9c56150db7697a21ac106c2c75337b99
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-01-27 17:18:57 +1100

    renames sheets certificate and token to accurately describe their nature, updates sheets_service_getter with filepath variables same as docs_service_getter

--- google_api_service_getters/sheets_service_getter.py
+++ google_api_service_getters/sheets_service_getter.py
@@ -13,21 +13,26 @@ def get_credentials():
 
     scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
+    path_to_token = 'credentials_tokens/token_sheets.json'
+
+    path_to_credentials = 'credentials_tokens/credentials_sheets.json'
+
+
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    if os.path.exists(path_to_token):
+        creds = Credentials.from_authorized_user_file(path_to_token, scopes)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', scopes)
+                path_to_credentials, scopes)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(path_to_token, 'w') as token:
             token.write(creds.to_json())
     
     return creds
@@ -39,3 +44,13 @@ def sheets_service_getter(spreadsheet_id):
     service = build('sheets', 'v4', credentials = creds)
 
     return service
+
+def main():
+
+    spreadsheet_id = '1OMDtvdnsu0kFCXFxQnSRcP49qpIiWrosUSV1si5OKPg'
+
+    service = sheets_service_getter(spreadsheet_id)
+
+    print(service)
+
+main()
