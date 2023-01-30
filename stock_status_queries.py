3d6b424b938bb2d924c934c6432d7be78925c145
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-01-30 23:35:50 +1100

    removes main() function which was causing weird execution problems with panel

--- stock_status_queries.py
+++ stock_status_queries.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+import hvplot.pandas
+
 from menzies_stock_info import menzies_stock_info
 
 from google_api_service_getters.sheets_service_getter import sheets_service_getter
@@ -54,21 +56,3 @@ def dataframe_queries(df):
     low_btb_df = df.query(low_btb_query).sort_values(by=['soh'], ascending=True)
 
     return oos_btg_df, low_btg_df, oos_btb_df, low_btb_df
-
-def main():
-
-    spreadsheet_id, restocked_values_range, stock_status_range = menzies_stock_info()
-
-    service = sheets_service_getter(spreadsheet_id)
-
-    stock_status_df = stock_status_df_builder(spreadsheet_id, service, stock_status_range)
-
-    oos_btg_df, low_btg_df, oos_btb_df, low_btb_df = dataframe_queries(stock_status_df)
-
-    print(oos_btg_df[['soh', 'par', 'soh_ratio']])
-
-    print(low_btg_df[['soh', 'par', 'soh_ratio']])
-
-    print(low_btb_df[['soh', 'par', 'soh_ratio']])
-
-main()
